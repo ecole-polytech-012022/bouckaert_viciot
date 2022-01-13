05bd05f666f9951c0695fc5a74550a6f35abf072
Benoît <b.bouckaert0@gmail.com>
2022-01-13 11:45:29 +0100

    Initial commit (2) : previous error ...

---
+++ src/main/java/com/polytech/LBChess/LbChessApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.LBChess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbChessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/LBChess/LbChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.LBChess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
